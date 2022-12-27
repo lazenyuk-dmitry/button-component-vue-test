54ad866803d8abb46a5cf1d49f6f55579c6dd3a4
Dmitry Lazenyuk <lazenyukdima@gmail.com>
2022-12-27 15:58:24 +0600

    Create button color prop for set colors.

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -1,12 +1,39 @@
-<script setup></script>
-
 <template>
-  <button :class="$style.btn" type="button">
+  <button :class="[$style.btn, colorClass]" type="button" v-bind="$attrs">
     <slot />
   </button>
 </template>
 
+<script>
+export default {
+  name: "AppButton",
+  props: {
+    color: {
+      type: String,
+      default: "primary",
+      validator(value) {
+        return [
+          "primary",
+          "secondary",
+          "warning",
+          "info",
+          "danger",
+          "action",
+        ].includes(value);
+      },
+    },
+  },
+  computed: {
+    colorClass() {
+      return this.$style[`btn--${this.color}`];
+    },
+  },
+};
+</script>
+
 <style lang="scss" module>
+$transition-time: 0.3s;
+
 .btn {
   padding: 14px 45px;
   background: $color-primary;
@@ -22,5 +49,38 @@
   display: inline-flex;
   justify-content: center;
   align-items: center;
+  transition: $transition-time;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+    transition: $transition-time;
+  }
+
+  &--primary {
+    background: $color-primary;
+  }
+  &--secondary {
+    background: $color-secondary;
+  }
+  &--warning {
+    background: $color-warning;
+  }
+  &--info {
+    background: $color-info;
+  }
+  &--danger {
+    background: $color-danger;
+  }
+  &--action {
+    background: $color-action;
+  }
+  &:disabled {
+    background: $color-disabled;
+    color: $btn-text-color-second;
+    cursor: not-allowed;
+    opacity: 1;
+    transition: $transition-time;
+  }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,9 +4,26 @@ import AppButton from "@/components/AppButton.vue";
 
 <template>
   <main>
-    <h3>button classic порпоррор</h3>
-    <div>
-      <AppButton>Отправить письмо</AppButton>
+    <h3>button classic</h3>
+    <div :class="$style.buttonContainer">
+      <AppButton :class="$style.btn">Отправить письмо</AppButton>
+      <AppButton :class="$style.btn" color="secondary">Отправить письмо</AppButton>
+      <AppButton :class="$style.btn" color="warning">Отправить письмо</AppButton>
+      <AppButton :class="$style.btn" disabled>Отправить письмо</AppButton>
+      <AppButton :class="$style.btn" color="info">Отправить письмо</AppButton>
+      <AppButton :class="$style.btn" color="danger">Отправить письмо</AppButton>
+      <AppButton :class="$style.btn" color="action">Отправить письмо</AppButton>
     </div>
   </main>
 </template>
+
+<style lang="scss" module>
+.buttonContainer {
+  display: flex;
+  flex-wrap: wrap;
+
+  .btn {
+    margin: 20px;
+  }
+}
+</style>
